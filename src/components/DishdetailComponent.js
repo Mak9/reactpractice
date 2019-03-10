b62b404bdfd913d1cfa920d7efc35f73028f0ebc
Mahim Khan <9khanmahim@gmail.com>
2019-03-11 00:10:44 +0530

    Assignment #3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,37 +5,46 @@ class DishDetail extends Component {
         super(props);
         console.log(props)
     }
+renderComments(){
+    const com=this.props.detail.comments.map((c)=>{
+        return(
+            <div key={c.id}>
+                
+                <p>{c.comment}</p>
+                <p>--{c.author} , {c.date}</p>
 
+            </div>
+        )
+    }
+    
+    );
+    return com
 
-
-    render() {
-        const com=this.props.detail.comments.map((c)=>{
-            return(
-                <div key={c.id}>
-                    
-                    <p>{c.comment}</p>
-                    <p>--{c.author} , {c.date}</p>
-
-                </div>
-            )
-        }
-        );
-        console.log(this.props.detail.comments)
-        return (
-            //<div><p>{this.props.detail.name}</p><br></br><p>{this.props.detail.description}</p></div>
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
+}
+renderDish(){
+    return(
+        <Card>
                         <CardImg top width="100%" src={this.props.detail.image} alt="Card image cap" />
                         <CardBody>
                             <CardTitle>{this.props.detail.name}</CardTitle>
                             <CardText>{this.props.detail.description}</CardText>
                         </CardBody>
-                    </Card>
+        </Card>
+    )
+}
+
+    render() {
+        const cmm=this.renderComments()
+        console.log(this.props.detail.comments)
+        return (
+            //<div><p>{this.props.detail.name}</p><br></br><p>{this.props.detail.description}</p></div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderDish()}
                 </div>
                 <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
-                    {com}
+                    {cmm}
                 </div>
             </div>
 
